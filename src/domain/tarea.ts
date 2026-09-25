ac3938575940323b96d6b0f7147b7e55d7e0d9ec
alosadasei <alosadasei@gmail.com>
2026-09-25 17:52:12 +0200

    Crear crearTarea con estado pendiente por defecto

--- src/domain/tarea.ts
+++ src/domain/tarea.ts
@@ -4,3 +4,12 @@ export type Tarea = {
   estado: 'pendiente' | 'en-curso' | 'hecha';
   prioridad: number;
 };
+
+export function crearTarea(id: number, titulo: string, prioridad: number): Tarea {
+  return {
+    id,
+    titulo,
+    estado: 'pendiente',
+    prioridad,
+  };
+}
